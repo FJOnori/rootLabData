e8683f97996a768d75f0865122a4291fee648747
FJOnori <s1960329@ed.ac.uk>
2026-02-06 15:02:50 +0000

    new runs

--- hist.cpp
+++ hist.cpp
@@ -6,33 +6,49 @@
 #include <TTree.h>
 #include <TBranch.h>
 #include <iostream>
+#include <array>
 
-void hist(std::string feature = "B01_PMT1_ADC", std::string fileName = "data/out-feb-45F6379E-000345.root"){
+void hist(){
+    // drawing of the set up and pictures of labels to draw
+    // take pictures
+    // 100 bins 0-500 range
+    // 346 NaI, 347 LSO
 
-    int size = 8;
+    std::string feature      = "B01_PMT1_ADC";
+    std::string fileNames[2] = {"data/out-feb-45F6379E-000344.root","data/out-feb-4944044F-000344.root"};
+    int colors[2] = {632, 400};
 
-    TFile *inFile = TFile::Open(fileName.c_str(), "READ");
-    TTree *inTree = (TTree*)inFile->Get("mdfTree;1");
+    int size  = 8;
+    int files = 2;
 
-    UShort_t featureAddress[size][size];
-    TH1F *hist[size][size];
-    int numOfEntries = inTree->GetEntries();
-    inTree->SetBranchAddress(feature.c_str(), featureAddress);
+    TH1F *hist[files][size][size];
 
-    for (int x = 0; x < size; x++){
-        for (int y = 0; y < size; y++){
+    for (int f = 0; f < files; f++){
 
-            Double_t xmin = inTree->GetMinimum(feature.c_str());
-            Double_t xmax = inTree->GetMaximum(feature.c_str());
+        std::string fileName = fileNames[f];
 
-            hist[x][y] = new TH1F(Form("hist_%d_%d", x, y), Form("%s_%d_%d", feature.c_str(), x+1, y+1), xmin, xmax, 20);
+        TFile *inFile = TFile::Open(fileName.c_str(), "READ");
+        TTree *inTree = (TTree*)inFile->Get("mdfTree;1");
 
-            for (int entryIndex = 0; entryIndex < numOfEntries; entryIndex++) {
-                inTree->GetEntry(entryIndex);
-                UShort_t entry  = featureAddress[x][y];
-                hist[x][y]->Fill(entry);
+        UShort_t featureAddress[size][size];
+        
+        int numOfEntries = inTree->GetEntries();
+        inTree->SetBranchAddress(feature.c_str(), featureAddress);
+
+        for (int x = 0; x < size; x++){
+            for (int y = 0; y < size; y++){
+
+                hist[f][x][y] = new TH1F(Form("hist_%d_%d", x, y), Form("%s_%d_%d", feature.c_str(), x+1, y+1), 5, 20, 5);
+
+                for (int entryIndex = 0; entryIndex < numOfEntries; entryIndex++) {
+                    inTree->GetEntry(entryIndex);
+                    UShort_t entry  = featureAddress[x][y];
+                    hist[f][x][y]->Fill(entry);
+                }
+
             }
         }
+
     }
 
     int topX       = 200;
@@ -41,20 +57,25 @@ void hist(std::string feature = "B01_PMT1_ADC", std::string fileName = "data/out
     int height     = 400;
     float margins  = 0.001;
     auto *canvas = new TCanvas("canvas", "Signal Example", topX, topY, width, height);
-    
+
     canvas->Divide(size, size);
 
-    for (int x = 0; x < size; x++) {
-        for (int y = 0; y < size; y++) {
-            canvas->cd((y*size + x)+1);
-            gPad->SetLeftMargin(margins);
-            gPad->SetRightMargin(margins);
-            gPad->SetTopMargin(margins);
-            gPad->SetBottomMargin(margins);
-            hist[x][y]->Draw();
+    for (int f = 0; f < files; f++){
+        for (int x = 0; x < size; x++) {
+            for (int y = 0; y < size; y++) {
+                
+                canvas->cd((y*size + x)+1);
+                gPad->SetLeftMargin(margins);
+                gPad->SetRightMargin(margins);
+                gPad->SetTopMargin(margins);
+                gPad->SetBottomMargin(margins);
+                hist[f][x][y]->SetLineColor(colors[f]);
+                hist[f][x][y]->Draw("SAME");
+            }
         }
     }
 
+ 
     canvas->Update();
 
 }
